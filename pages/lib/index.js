3187cf31c78092172e628bcb77c4703a967203b3
Croustys <akos.barabas2002@gmail.com>
2021-11-19 14:50:09 +0100

    first commit

---
+++ pages/lib/index.js
@@ -0,0 +1,7 @@
+import dataJson0 from "../../public/data/StreamingHistory0.json";
+import dataJson1 from "../../public/data/StreamingHistory1.json";
+import dataJson2 from "../../public/data/StreamingHistory2.json";
+export default function dataFetch() {
+  const merged = [...dataJson0, ...dataJson1, ...dataJson2];
+  return merged;
+}
